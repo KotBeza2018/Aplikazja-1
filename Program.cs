64ef26340b86f31d3e30a19cb56a5b1102417405
Blazej <dymekblazej@gmail.com>
2020-10-14 19:46:58 +0200

    Zajecia 1

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplikazja_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tekst = "Beza";
+            Console.WriteLine(tekst);
+            tekst = Console.ReadLine();
+            Console.WriteLine(tekst);
+            //Console.ReadLine();
+            Main(args);
+
+        }
+    }
+}
