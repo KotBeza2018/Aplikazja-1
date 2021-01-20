a788aff12eef6617b7eac0f2cd139b526bddb03f
uczen <uczen@ITP-D-05>
2021-01-20 19:23:46 +0100

    20.01.2021

--- Aplikacja2 Szyfrator/MainWindow.Designer.cs
+++ Aplikacja2 Szyfrator/MainWindow.Designer.cs
@@ -28,12 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.Tytuł = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.OpenButton = new System.Windows.Forms.Button();
             this.SaveButton = new System.Windows.Forms.Button();
             this.text = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.passwordsDataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.passwordsDataGridView)).BeginInit();
             this.SuspendLayout();
             // 
             // Tytuł
@@ -41,7 +45,7 @@
             this.Tytuł.AutoSize = true;
             this.Tytuł.Font = new System.Drawing.Font("Arial Rounded MT Bold", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Tytuł.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
-            this.Tytuł.Location = new System.Drawing.Point(425, 9);
+            this.Tytuł.Location = new System.Drawing.Point(431, 9);
             this.Tytuł.Name = "Tytuł";
             this.Tytuł.Size = new System.Drawing.Size(320, 75);
             this.Tytuł.TabIndex = 0;
@@ -54,7 +58,7 @@
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
-            this.label1.Location = new System.Drawing.Point(353, 101);
+            this.label1.Location = new System.Drawing.Point(362, 84);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(437, 55);
             this.label1.TabIndex = 1;
@@ -63,10 +67,10 @@
             // OpenButton
             // 
             this.OpenButton.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.OpenButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
-            this.OpenButton.Location = new System.Drawing.Point(588, 434);
+            this.OpenButton.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.OpenButton.Location = new System.Drawing.Point(438, 170);
             this.OpenButton.Name = "OpenButton";
-            this.OpenButton.Size = new System.Drawing.Size(420, 110);
+            this.OpenButton.Size = new System.Drawing.Size(313, 110);
             this.OpenButton.TabIndex = 2;
             this.OpenButton.Text = "open";
             this.OpenButton.UseVisualStyleBackColor = true;
@@ -75,10 +79,10 @@
             // SaveButton
             // 
             this.SaveButton.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.SaveButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
-            this.SaveButton.Location = new System.Drawing.Point(151, 434);
+            this.SaveButton.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.SaveButton.Location = new System.Drawing.Point(108, 170);
             this.SaveButton.Name = "SaveButton";
-            this.SaveButton.Size = new System.Drawing.Size(420, 110);
+            this.SaveButton.Size = new System.Drawing.Size(313, 110);
             this.SaveButton.TabIndex = 3;
             this.SaveButton.Text = "zapisz";
             this.SaveButton.UseVisualStyleBackColor = true;
@@ -87,23 +91,53 @@
             // text
             // 
             this.text.BackColor = System.Drawing.Color.Teal;
-            this.text.Location = new System.Drawing.Point(151, 168);
+            this.text.Font = new System.Drawing.Font("Miriam Mono CLM", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.text.Location = new System.Drawing.Point(769, 286);
             this.text.Multiline = true;
             this.text.Name = "text";
-            this.text.Size = new System.Drawing.Size(857, 244);
+            this.text.Size = new System.Drawing.Size(367, 259);
             this.text.TabIndex = 4;
             this.text.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // button1
             // 
-            this.button1.ForeColor = System.Drawing.Color.Olive;
-            this.button1.Location = new System.Drawing.Point(132, 33);
+            this.button1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Teal;
+            this.button1.Location = new System.Drawing.Point(769, 170);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(183, 91);
+            this.button1.Size = new System.Drawing.Size(367, 110);
             this.button1.TabIndex = 5;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // passwordsDataGridView
+            // 
+            this.passwordsDataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.passwordsDataGridView.BackgroundColor = System.Drawing.Color.DodgerBlue;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.passwordsDataGridView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.passwordsDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.passwordsDataGridView.DefaultCellStyle = dataGridViewCellStyle2;
+            this.passwordsDataGridView.EnableHeadersVisualStyles = false;
+            this.passwordsDataGridView.GridColor = System.Drawing.Color.Teal;
+            this.passwordsDataGridView.Location = new System.Drawing.Point(108, 286);
+            this.passwordsDataGridView.Name = "passwordsDataGridView";
+            this.passwordsDataGridView.Size = new System.Drawing.Size(643, 259);
+            this.passwordsDataGridView.TabIndex = 6;
             // 
             // MainWindow
             // 
@@ -111,6 +145,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Pink;
             this.ClientSize = new System.Drawing.Size(1183, 660);
+            this.Controls.Add(this.passwordsDataGridView);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.text);
             this.Controls.Add(this.SaveButton);
@@ -126,6 +161,7 @@
             this.TransparencyKey = System.Drawing.Color.Black;
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainWindow_FormClosed);
             this.Load += new System.EventHandler(this.MainWindow_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.passwordsDataGridView)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -140,6 +176,7 @@
         private System.Windows.Forms.TextBox text;
         private System.Windows.Forms.Button szyfruj;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView passwordsDataGridView;
     }
 }
 
